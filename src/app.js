4606a0fc5522f3e594b7b0138ae2959a0f8b0218
ps8847 <ps789650@gmail.com>
2022-11-09 21:40:40 +0530

    modifications for new hosting

--- src/app.js
+++ src/app.js
@@ -3,55 +3,56 @@ const app = express();
 const hbs = require("hbs");
 const port = process.env.PORT || 8000;
 
+const serverless = require("serverless-http");
 
 // public sttaic path
 const path = require("path");
 
-const staticPath = path.join(__dirname, "../public")
-const template_path = path.join(__dirname, "../templates/views")
-const partials_path = path.join(__dirname, "../templates/partials")
+const staticPath = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
 app.set("view engine", "hbs");
 app.set("views", template_path);
 hbs.registerPartials(partials_path);
 
-app.use(express.static(staticPath))
-
-
+app.use(express.static(staticPath));
 
 // routing
 app.get("/", (req, res) => {
-    // res.send("Welcome to WEATHER APP PAGE");
-    res.render("index")
-})
+  // res.send("Welcome to WEATHER APP PAGE");
+  res.render("index");
+});
 
 app.get("/about", (req, res) => {
-    // res.send("Welcome to WEATHER APP's <b>about us</b> page");
-    res.render("about")
-})
+  // res.send("Welcome to WEATHER APP's <b>about us</b> page");
+  res.render("about");
+});
 
 app.get("/about/*", (req, res) => {
-    // res.send("Welcome to WEATHER APP's <b>about us</b> page");
-    res.render("404error")
-})
+  // res.send("Welcome to WEATHER APP's <b>about us</b> page");
+  res.render("404error");
+});
 
 app.get("/weather", (req, res) => {
-    // res.send("Welcome to WEATHER APP's weather page");
-    res.render("weather");
+  // res.send("Welcome to WEATHER APP's weather page");
+  res.render("weather");
 });
 
 app.get("/weather/*", (req, res) => {
-    // res.send("Welcome to WEATHER APP's weather page");
-    res.render("404error");
+  // res.send("Welcome to WEATHER APP's weather page");
+  res.render("404error");
 });
 
 app.get("*", (req, res) => {
-    // res.send("404 error page!! ");
-    res.render("404error", {
-        errormsg: "The Page Your are Searching for Not Exist!!"
-    });
+  // res.send("404 error page!! ");
+  res.render("404error", {
+    errormsg: "The Page Your are Searching for Not Exist!!",
+  });
 });
 
 app.listen(port, () => {
-    console.log(`listeninh to port no : ${port}`);
+  console.log(`listeninh to port no : ${port}`);
 });
+
+module.exports.handler = serverless(app);
